1b2ab2f7e0136a0b775731dafd356485989cefa1
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-27 22:07:44 +0530

    Add files via upload
    Final Commit

--- Main.java
+++ Main.java
@@ -1,100 +1,19 @@
-package com.kunal.introduction;
+package com.kunal.singleton;
 
-import java.sql.Struct;
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.kunal.access.A;
 
 public class Main {
     public static void main(String[] args) {
-        // store 5 roll nos
-        int[] numbers = new int[5];
+        Singleton obj1 = Singleton.getInstance();
 
-        // store 5 names
-        String[] names = new String[5];
+        Singleton obj2 = Singleton.getInstance();
 
-        // data of 5 students: {roll no, name, marks}
-        int[] rno = new int[5];
-        String[] name = new String[5];
-        float[] marks = new float[5];
+        Singleton obj3 = Singleton.getInstance();
 
-        Student[] students = new Student[5];
+        // all 3 ref variables are pointing to just one object
 
-        // just declaring
-//        Student kunal;
-//        kunal = new Student();
-
-        Student kunal = new Student(15, "Kunal Kushwaha", 85.4f);
-        Student rahul = new Student(18, "Rahul Rana", 90.3f);
-
-//        kunal.rno = 13;
-//        kunal.name = "Kunal Kushwaha";
-//        kunal.marks = 88.5f;
-
-
-//        kunal.changeName("Shoe lover");
-//        kunal.greeting();
-
-//
-        System.out.println(kunal.rno);
-        System.out.println(kunal.name);
-        System.out.println(kunal.marks);
-
-        Student random = new Student(kunal);
-        System.out.println(random.name);
-
-        Student random2 = new Student();
-        System.out.println(random2.name);
-
-        Student one = new Student();
-        Student two = one;
-
-        one.name = "Something something";
-
-        System.out.println(two.name);
-
+        A a = new A(10, "Kunal");
+        a.getNum();
+//        int n = a.num;
     }
 }
-
-// create a class
-// for every single student
-class Student {
-    int rno;
-    String name;
-    float marks = 90;
-
-    // we need a way to add the values of the above
-    // properties object by object
-
-    // we need one word to access every object
-
-    void greeting() {
-        System.out.println("Hello! My name is " + this.name);
-    }
-
-    void changeName(String name) {
-        this.name = name;
-    }
-
-    Student  (Student other) {
-        this.name = other.name;
-        this.rno = other.rno;
-        this.marks = other.marks;
-    }
-
-    Student () {
-        // this is how you call a constructor from another constructor
-        // internally: new Student (13, "default person", 100.0f);
-        this (13, "default person", 100.0f);
-    }
-
-
-    // Student arpit = new Student(17, "Arpit", 89.7f);
-    // here, this will be replaced with arpit
-    Student (int rno, String name, float marks) {
-        this.rno = rno;
-        this.name = name;
-        this.marks = marks;
-    }
-
-}
-
