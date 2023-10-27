b4920a32ecc846e0c6b5dd10966b0a5b8560d6e3
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-27 22:09:22 +0530

    Add files via upload
    Initial Commit

--- Greeting.java
+++ Greeting.java
@@ -1,10 +1,7 @@
-package com.kunal.packages.a;
-
-import static com.kunal.packages.b.Message.message;
+package com.kunal.packages.b;
 
 public class Greeting {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        message();
+        System.out.println(" I am awesome");
     }
 }
