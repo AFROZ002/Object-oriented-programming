f788a81220d707a86e3b47381cb24068ebb800d0
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-28 23:37:26 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,40 @@
-package com.kunal.access;
-
-import java.util.ArrayList;
+package com.kunal.staticExample;
 
 public class Main {
     public static void main(String[] args) {
-        A obj = new A(10, "Kunal");
-        // need to do a few things
-        // 1. access the data members
-        // 2. modify the data members
+//        Human kunal = new Human(22, "Kunal", 10000, false);
+//        Human rahul = new Human(34, "Rahul", 15000, true);
+//        Human arpit = new Human(34, "arpit", 15000, true);
+//
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
+//        System.out.println(Human.population);
 
-//        ArrayList<Integer> list = new ArrayList<>(23);
-//        list.DEFAULT_CAPACITY;
+        Main funn = new Main();
+        funn.fun2();
 
-        obj.getNum();
-        int n = obj.num;
+    }
+
+    // this is not dependent on objects
+    static void fun() {
+//        greeting(); // you cant use this because it requires an instance
+        // but the function you are using it in does not depend on instances
+
+        // you cannot access non static stuff without referencing their instances in
+        // a static context
+
+        // hence, here I am referencing it
+        Main obj = new Main();
+        obj.greeting();
+    }
+
+    void fun2() {
+        greeting();
+    }
+
+    // we know that something which is not static, belongs to an object
+    void greeting() {
+//        fun();
+        System.out.println("Hello world");
     }
 }
