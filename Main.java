80cd203cbca70475d63967b964a5029aaba84417
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-30 23:03:46 +0530

    Add files via upload
    initial commit

--- Main.java
+++ Main.java
@@ -1,40 +1,16 @@
-package com.kunal.staticExample;
+package com.kunal.abstractDemo;
 
 public class Main {
     public static void main(String[] args) {
-//        Human kunal = new Human(22, "Kunal", 10000, false);
-//        Human rahul = new Human(34, "Rahul", 15000, true);
-//        Human arpit = new Human(34, "arpit", 15000, true);
-//
-//        System.out.println(Human.population);
-//        System.out.println(Human.population);
-//        System.out.println(Human.population);
+        Son son = new Son(30);
+        son.career();
 
-        Main funn = new Main();
-        funn.fun2();
+        son.normal();
 
-    }
+        Parent daughter = new Daughter(28);
+        daughter.career();
 
-    // this is not dependent on objects
-    static void fun() {
-//        greeting(); // you cant use this because it requires an instance
-        // but the function you are using it in does not depend on instances
-
-        // you cannot access non static stuff without referencing their instances in
-        // a static context
-
-        // hence, here I am referencing it
-        Main obj = new Main();
-        obj.greeting();
-    }
-
-    void fun2() {
-        greeting();
-    }
-
-    // we know that something which is not static, belongs to an object
-    void greeting() {
-//        fun();
-        System.out.println("Hello world");
+        Parent.hello();
+//        Parent mom = new Parent(45);
     }
 }
