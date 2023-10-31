86e38ad2571c92322f8e0e570dd4d09301a88001
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-31 23:06:34 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,5 +1,13 @@
-package com.kunal.interfaces.extendDemo;
+package com.kunal.interfaces.extendDemo2;
 
 public interface A {
-    void fun();
+    // static interface methods should always have a body
+    // call via the interface name
+    static void greeting() {
+        System.out.println("Hey I am static method");
+    }
+
+    default void fun() {
+        System.out.println("I am in A");
+    }
 }
