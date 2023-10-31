435d8d70322955d4b8146fb2a44cfd5821bb7c56
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-10-31 23:05:20 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,21 +1,5 @@
-package com.kunal.access;
+package com.kunal.interfaces.extendDemo;
 
-public class A {
-    protected int num;
-    String name;
-    int[] arr;
-
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public A(int num, String name) {
-        this.num = num;
-        this.name = name;
-        this.arr = new int[num];
-    }
+public interface A {
+    void fun();
 }
